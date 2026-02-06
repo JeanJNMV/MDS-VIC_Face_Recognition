925523de445a67a5fcb0680a8c7fcd434fd1a0f3
Fotis <fotiskapotos@gmail.com>
2026-02-06 17:29:08 +0100

    Eigenfaces stuff

--- src/vic/models.py
+++ src/vic/models.py
@@ -209,13 +209,15 @@ class TinyCNN(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
 
         self.pool = nn.MaxPool2d(2, 2)
+        self.adapt = nn.AdaptiveAvgPool2d((7, 7))
 
-        self.fc1 = nn.Linear(32 * 28 * 23, 128)  # ORL size: 112×92 → /4
+        self.fc1 = nn.Linear(32 * 7 * 7, 128)  # ORL size: 112×92 → /4
         self.fc2 = nn.Linear(128, n_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.adapt(x)
         x = x.flatten(1)
         x = F.relu(self.fc1(x))
         return self.fc2(x)
